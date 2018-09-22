79587af3d82b6a9c5f4b602253bed83892308604
Michael Kim <m.kim4247@gmail.com>
2018-09-22 06:46:33 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(array, index)
-  if array[index] == " " || array[index] == ""
+  if array[index] == " " || array[index] == "" || nil
     return false 
   end
 end
